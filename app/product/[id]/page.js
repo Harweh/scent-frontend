05b656c02889865fbc72e45de1cf49248c78cb46
Emmy <vickyboi229@gmail.com>
2026-08-09 07:09:04 +0100

    updating

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -133,7 +133,6 @@ export default function ProductDetailPage() {
                     {emoji}
                 </div>
                 )}
-                
             </div>
 
             {/* RIGHT — Info */}
